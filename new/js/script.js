862ad594e084e8ca10aeb59a10323cd7fa1bc6d7
Jakob Anderson <ficitonisfiction@gmail.com>
2012-07-30 17:00:41 -0600

    daily end patch

--- new/js/script.js
+++ new/js/script.js
@@ -2,10 +2,32 @@
 /*  powered by Google Maps API v3   */
 /*  Revised:  07.30.2012            */
 
+
+
 // Init
     // Show Loading Animation (progress?)
     // Detect Device/Size
+    
+    // Set map default options
+    var myLatlng = new google.maps.LatLng( 43.815045, -111.783515);
+    var myOptions = {
+      //maxZoom: 18,
+      zoom: 16,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 
+                     google.maps.MapTypeId.SATELLITE, 
+                     google.maps.MapTypeId.HYBRID, 
+                     google.maps.MapTypeId.TERRAIN]
+      }//end mapTypeControlOptions
+    };//end myOptions
+
     // Create Map Object
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+    }//end initialize()
+
     // Create infoWindow Object
     // Load Campus Boundary Layer
     // run GatherData Stack
@@ -17,3 +39,7 @@
   // ShowHideCategory
   // ToggleMobileDesktop
   // Search
+  // Resize map_canvas to fit screen
+  // (function(){
+  //   window.getOffset
+  // })
