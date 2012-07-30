203d8da1d7d2ae42ac6262f9a0b08f5a2c3564d1
Jakob Anderson <ficitonisfiction@gmail.com>
2012-07-30 16:47:13 -0600

    small patch, past new bootstrap stage

--- new/js/script.js
+++ new/js/script.js
@@ -1,3 +1,19 @@
 /*  BYU-I Campus Map                */
 /*  powered by Google Maps API v3   */
 /*  Revised:  07.30.2012            */
+
+// Init
+    // Show Loading Animation (progress?)
+    // Detect Device/Size
+    // Create Map Object
+    // Create infoWindow Object
+    // Load Campus Boundary Layer
+    // run GatherData Stack
+    // run LoadPopulateShowCategories Stack
+    // Hide Loading Animation
+
+  // GatherData
+  // LoadPopulateShowCategories
+  // ShowHideCategory
+  // ToggleMobileDesktop
+  // Search
