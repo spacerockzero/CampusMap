206d8b2adc42123e805a1ec9ed4a6cf3b311d5ee
Matthew Sessions <shichgonrui@gmail.com>
2013-08-09 16:22:39 -0600

    Nightly
    Now shows everything, allows you to tell if you want the menus, and
    categories show up in the menu and you can toggle them.  Only problem
    right now is the response time on the events is very slow.

--- campus-map/Prebuilt/maps/js/newScript.js
+++ campus-map/Prebuilt/maps/js/newScript.js
@@ -13,22 +13,107 @@ function Category() {
 	this.link = arguments[6],
 	this.markerLocations = [],
 	this.polygonLocations = [],
+	this.state = 0,
+	this.element,
+	this.globals = arguments[7];
 	this.state = 0;
+
+	//need to create the Category DOM object, it's html and event listener
+	this.buildCatDOM();
+}
+//creates an element, builds the html and attaches an event listener to the category
+Category.prototype.buildCatDOM = function() {
+	  this.element = this.globals.doc.createElement("a");
+	  this.element.className = 'category_bar';
+	  this.element.setAttribute('href', '#');
+      this.element.innerHTML += '<img class="cat_icon" src="Prebuilt/maps/imgs/icons/blank-colors/'+ this.iconColor + '.png" />';
+      this.element.innerHTML += '<span class="category_name">' + this.title + '</span>';
+      this.bindEventListener();
+}
+Category.prototype.getCatDOMObj = function() {
+	var catObj = this.globals.doc.createElement('div');
+	catObj.className = 'category';
+	catObj.appendChild(this.element);
+
+	var catContainer = this.globals.doc.createElement('div');
+	catContainer.className = "cat_container";
+	catContainer.innerHTML = '<div class="cat_info"><p>' + this.text + '</p><a href="' + this.link + '" target="_blank">' + this.title + ' website</a></div>';
+
+	var objContainer = this.globals.doc.createElement('div');
+	//append each object element and polygon element
+	objContainer = this.appendLocations(objContainer);
+	objContainer = this.appendAreas(objContainer);
+
+	//append them all togethor and return the catObj DOM object
+	catContainer.appendChild(objContainer);
+	catObj.appendChild(catContainer);
+	return catObj;
+}
+//gets the html for a location
+Category.prototype.appendLocations = function(container) {
+	for (var i = 0, len = this.markerLocations.length; i < len; i++) {
+		container.appendChild(this.markerLocations[i].element);
+	}
+	return container;
+}
+Category.prototype.appendAreas = function(container) {
+	for (var i = 0, len = this.polygonLocations.length; i < len; i++) {
+		container.appendChild(this.polygonLocations[i].element);
+	}
+	return container;
+}
+//bind the event listener to the element
+Category.prototype.bindEventListener = function() {
+	var cat = this;
+	this.element.addEventListener('click', function() {
+		cat.toggle();
+	});
+}
+Category.prototype.toggle = function() {
+	var sibling = this.element.parentElement.children[1];
+	if (this.state === 0) {
+		//open the category
+		sibling.style.height = "100%";
+		this.state = 1;
+	} else {
+		//close the category
+		sibling.style.height = "0";
+		this.state = 0;
+	}
 }
 
 //the location class definition is for use by markers
 //uses the arguments parameter
 function Location() {
+	this.number = arguments[8],
 	this.name = arguments[0],
 	this.code = arguments[1],
 	this.lat = arguments[2],
 	this.lon = arguments[3],
 	this.img = arguments[4],
-	this.hours = arguments[4],
-	this.info = arguments[5],
-	this.link = arguments[6],
-	this.marker;
+	this.hours = arguments[5],
+	this.info = arguments[6],
+	this.link = arguments[7],
+	this.element,
+	this.marker,
+	this.infoWindowHTML,
+	this.globals = arguments[9];
+
+	this.buildLocationDOM();
 }
+Location.prototype.buildLocationDOM = function() {
+	this.element = this.globals.doc.createElement("a");
+	this.element.className = "object marker_object";
+	this.element.name = this.name;
+	this.element.setAttribute("href", "#" + this.code);
+    this.element.innerHTML += '<img  class="obj_icon" src="Prebuilt/maps/imgs/icons/numeral-icons/' + this.color + '/' + this.number + '.png" alt="' + name + '" />';
+    this.element.innerHTML +=   '<div class="object_name">' + this.name + '</div>';
+    this.bindEventListener();
+}
+Location.prototype.bindEventListener = function() {
+
+}
+
 
 //the area class definition for use for polygons
 function Area() {
@@ -38,7 +123,22 @@ function Area() {
 	this.borderColor = arguments[3],
 	this.fillColor = arguments[4],
 	this.map = arguments[5],
-	this.polygon;
+	this.polygon,
+	this.element,
+	this.globals = arguments[6];
+
+	this.buildAreaDOM();
+}
+Area.prototype.buildAreaDOM = function() {
+	this.element = this.globals.doc.createElement("a");
+	this.element.className = "object polygon";
+	this.element.setAttribute('href', '#' + this.code);
+	this.element.innerHTML = '<div class="polygon_key" id="poly_' + this.name + '" style="border-color:' + this.borderColor + '; background-color:' + this.fillColor + '"><span>&nbsp;</span></div>';
+    this.element.innerHTML += '<div class="object_name polygon">' + this.name + '</div>';
+    this.bindEventListener();
+}
+Area.prototype.bindEventListener = function() {
+
 }
 
 //this class is the definition for the campusMap class that has all of the code in it to initialize a campusMap
@@ -55,6 +155,7 @@ function CampusMap(options) {
 	this.element = (options['element']) ? options['element'] : console.log("No element provided.") ,
 	this.menuState = (options['menuState']) ? options['menuState'] : 1,
 	this.includeMenus = (options['includeMenus'] == null) ? true : options['includeMenus'],
+	this.device,
 	this.map = new Map(options), //send the whole options object so that it can get the attributes that are related to it
 	this.objectFile = 'Prebuilt/maps/data/newCatObj.JSON',
 	this.device = 0;
@@ -65,22 +166,36 @@ function CampusMap(options) {
 		doc: document,
 		win: window
 	}
-	addScript("http://maps.googleapis.com/maps/api/js?v=3&sensor=false&callback=campusMap.initializeMaps",{win: window, doc: document},function() {campusMap.initializeMaps();});
+	addScript("http://maps.googleapis.com/maps/api/js?v=3&sensor=false&callback=campusMap.initializeMaps",this.globals);
 }
+//calls the initialize map stack
 CampusMap.prototype.initializeMaps = function() {
 		console.time("initialize js chain until ready");
 		//make a copy of all of the globals
 		var local = this.globals;
-		//the first thing that we are going to want to do is to pull in the google maps api script
-		//we will place it at the bottom of the body
-		this.map.initiateMap(this.element, local);
-
+		//build the mark up based on the options sent in
+		this.buildHTML();
+		//initiate the maps
+		this.map.initiateMap(local);
+		if (this.includeMenus) {
+			this.detectDevice();
+		}
 		//load the category 
-		this.loadCatObjFile(function() {
-
-		})
-	}
-CampusMap.prototype.loadCatObjFile = function(callback) {
+		this.loadCatObjs();
+}
+//builds the needed HTML for the map
+CampusMap.prototype.buildHTML = function() {
+	var html = (this.includeMenus) ? '<div id="title"><h1 id="heading">BYU-Idaho Campus Map</h1><a id="device_type" href="#" onmousedown="toggleDevice(); return false;" title="Switch Device Type"><div id="device_container"><div class="device icon-desktop"></div><div class="device icon-mobile"></div></div></a><a id="menu_button" class="icon-settings" href="#" title="Open Menu. Click this or press [space] bar"></a></div>' : "";
+	html += '<div id="container" name="container">';
+	html += (this.includeMenus) ? '<div id="menu" name="menu" style="display:block; z-index: 2;"><div id="inner_menu" class="scrolling-element-class" ><div id="object_search"><input type="text" placeholder="Search"/><span class="icon-cancel"></span></div><nav id="categories" class="child-element"></nav><!-- // categories --></div><!-- // inner menu --></div><!-- // menu -->' : "";
+	html += '<div id="map_canvas"><div id="nojs-msg"><br/>This BYU-Idaho Campus Map application requires Javascript to run. <br/>Your device or browser doesn\'t appear to have JavaScript enabled. <br/>Please enable it and try again, or try another device or browser.</div></div>';
+	html += '<div id="map_keys"></div>';
+	this.globals.doc.getElementById(this.element).innerHTML = html;
+}
+//loads the category and object file and creates the categories and objects for each object
+CampusMap.prototype.loadCatObjs = function(callback) {
+	var filePath = this.objectFile;
+	var parent = this;
 		var xmlhttp;
 		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
   			xmlhttp=new XMLHttpRequest();
@@ -88,41 +203,62 @@ CampusMap.prototype.loadCatObjFile = function(callback) {
   			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   		}
 		xmlhttp.onreadystatechange=function() {
-  			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+  			if (xmlhttp.readyState==4) {
   				var data = JSON.parse(xmlhttp.responseText);
-  				this.parseData(data);
+  				parent.parseCategories(data);
   				if (callback && typeof(callback) === "function") {
   					callback();
   				}
     		}
   		}
-		xmlhttp.open("GET",this.objectFile,true);
+		xmlhttp.open("GET",filePath,true);
 		xmlhttp.send();
 	}
+	//parses the category file and creates category objects of them all
 CampusMap.prototype.parseCategories = function(data) {
-		for (var i = 0, numberCategories = data.length; i < numCategories; i++) {
+		for (var i = 0, numberCategories = data.length; i < numberCategories; i++) {
 			var cat = data[i];
-			this.categories.push(new Category(cat.id, cat.name, cat.title, cat.text, cat.iconColor, cat.type, cat.link));
-			this.categories.markerLocations = this.parseLocations(cat.objects);
-			this.categories.polygonLocations = this.parseAreas(cat.polygons);
+			this.categories.push(new Category(cat.id, cat.name, cat.title, cat.text, cat.icon, cat.type, cat.link, this.globals));
+			this.categories.markerLocations = (cat.objects) ? this.parseLocations(cat.objects) : null;
+			this.categories.polygonLocations = (cat.polygons) ? this.parseAreas(cat.polygons) : null;
+			this.globals.doc.getElementById('categories').appendChild(this.categories[i].getCatDOMObj());
 		}
 	}
+	//parses all of the locations and creates location objects out of them all
 CampusMap.prototype.parseLocations = function(locations) {
 		var markerLocations = [];
 		for (var j = 0, numberLocations = locations.length; j < numberLocations; j++) {
 			var marker = locations[j]
-				markerLocations.push(new Location(marker.name, marker.code, marker.lat, marker.lon, marker.img, marker.hours, marker.info, marker.link));
+				markerLocations.push(new Location(marker.name, marker.code, marker.lat, marker.lon, marker.img, marker.hours, marker.info, marker.link, j, this.globals));
 			}
 		return markerLocations;
 	}
+	//parses all of the areas and creates area objects out of them all
 CampusMap.prototype.parseAreas = function(areas) {
 		var polygonAreas = [];
 		for (var j = 0, numberAreas = areas.length; j < numberAreas; j++) {
 			var polygon = areas[j]
-				polygonLocations.push(new Area(polygon.name, polygon.code, polygon.contains, polygon.borderColor, polygon.fillColor, polygon.map));
+				polygonAreas.push(new Area(polygon.name, polygon.code, polygon.contains, polygon.borderColor, polygon.fillColor, polygon.map, this.globals));
 			}
-		return polygonLocations;
+		return polygonAreas;
 	}
+	//detects what kind of device is being 
+CampusMap.prototype.detectDevice = function() {
+ var width = this.globals.doc.body.offsetWidth;
+    var newDevice = (width < 800) ? 0 : 1;
+    (newDevice != this.device) ? this.setDevice(this.device = newDevice) : null;
+}
+CampusMap.prototype.setDevice = function() {
+	var deviceIndicator = this.globals.doc.getElementById('device_type'),
+                   body = this.globals.doc.getElementsByTagName("body")[0];
+    if (this.device == 0){
+        // Set the body id, set new control value
+        body.setAttribute("id","mobile");
+    } else {
+        // Set the body id, set new control value
+        body.setAttribute("id","desktop");
+    }
+}
 
 //this class definition is for the maps, any interaction with the google maps goes through here
 function Map(options) {
@@ -137,9 +273,9 @@ function Map(options) {
 	this.infoWindow,
 	this.campusLayer;
 }
-Map.prototype.initiateMap = function(element, local) {
+Map.prototype.initiateMap = function(local) {
 		this.setGoogleMapOptions();
-		this.setGoogleMap(element, local);
+		this.setGoogleMap(local);
 		this.setInfoWindow();
 		this.setCampusLayer();
 	}
@@ -157,8 +293,8 @@ Map.prototype.setGoogleMapOptions = function() {
 			}
 		};
 	}
-Map.prototype.setGoogleMap = function(element, local) {
-		this.map = new google.maps.Map(local.doc.getElementById(element), this.googleMapOptions);
+Map.prototype.setGoogleMap = function(local) {
+		this.map = new google.maps.Map(local.doc.getElementById('map_canvas'), this.googleMapOptions);
 	}
 Map.prototype.setInfoWindow = function() {
 		this.infoWindow = new google.maps.InfoWindow();
